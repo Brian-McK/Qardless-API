d28dfb94de9575ebc62b4f237eb71a6319c9ccbc
Finbar <finbar.xp@gmail.com>
2022-12-10 16:15:56 +0000

    Created Initial API
    Includes Program.cs, dependencies, Models, Profiles and DTOs

---
+++ Qardless.API/Qardless.API/Profiles/CertificateProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Qardless.API.Dtos.Certificate;
+using Qardless.API.Models;
+
+namespace Qardless.API.Profiles
+{
+    public class CertificateProfile : Profile
+    {
+        public CertificateProfile()
+        {
+            // GET Full
+            CreateMap<Certificate, CertificateReadFullDto>();
+
+            // GET Partial
+            CreateMap<Certificate, CertificateReadPartialDto>();
+
+            // POST
+            CreateMap<CertificateCreateDto, Certificate>();
+
+            // PUT
+            CreateMap<CertificateUpdateDto, Certificate>();
+
+            // PATCH
+            CreateMap<Certificate, CertificateUpdateDto>();
+        }
+    }
+}
---
+++ Qardless.API/Qardless.API/Services/QardlessAPIContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Qardless.API.Models;
+
+namespace Qardless.API.Services
+{
+
+    /*
+     * NOTE: To create a Local SQL Server DB
+     *      1)  Download and install MS SQL Server Management Studio
+     *      2)  Open CMD or Powershell
+     *      3)  Write and run command "sqllocaldb create "Local"
+     *      4)  Open MS SQL Server Managment Studio
+     *      5)  Server name: "(localdb)\Local", Auth: Windows Auth
+     *      6)  In CMD or Powershell open the Qardless.API folder that
+     *          is beside the sln file.
+     *      7)  If there are no migrations:
+     *              Write and run command "dotnet ef migrations add InitialMigration"
+     *          Otherwise:
+     *              Write and run command "dotnet ef database update"
+     *      8)  You should then see the database and tables created in MS SQL Server Management Studio
+     */
+    public class QardlessAPIContext : DbContext
+    {
+        public QardlessAPIContext(DbContextOptions<QardlessAPIContext> opt) : base(opt)
+        {
+        }
+
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Business> Businesses { get; set; }
+        public DbSet<Certificate> Certificates { get; set; }
+        public DbSet<Changelog> Changelogs { get; set; }
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<EndUser> EndUsers { get; set; }
+        
+    }
+}
