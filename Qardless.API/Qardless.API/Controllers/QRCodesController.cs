80f22a4ca482b5fdf699491569af32ca6e449a8b
jacqueline <D00233669@student.dkit.ie>
2023-01-10 01:14:13 +0000

    created QR code controller

---
+++ Qardless.API/Qardless.API/Controllers/QRCodesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using QRCoder;
+using System.Drawing;
+using System.IO;
+
+namespace Qardless.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QRCodesController : ControllerBase
+    {
+        public byte[] ImageToByteArray(System.Drawing.Image img)
+        {
+            MemoryStream ms = new MemoryStream();
+            img.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+            return ms.ToArray();
+        }
+    }
+}
