460280fc1bd4ea408303feae25071c09cb598b78
jacqueline <D00233669@student.dkit.ie>
2023-01-10 22:56:51 +0000

    43207 updated packages

--- Qardless.API/Qardless.API/Controllers/QRCodesController.cs
+++ Qardless.API/Qardless.API/Controllers/QRCodesController.cs
@@ -3,19 +3,33 @@ using Microsoft.AspNetCore.Mvc;
 
 using QRCoder;
 using System.Drawing;
-using System.IO;
 
 namespace Qardless.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/qrcode")]
     [ApiController]
     public class QRCodesController : ControllerBase
     {
-        public byte[] ImageToByteArray(System.Drawing.Image img)
+        public byte[] ImageToByteArray(System.Drawing.Bitmap img)
         {
             MemoryStream ms = new MemoryStream();
             img.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
             return ms.ToArray();
+        }
+
+        [HttpGet("GenerateQRCode")]
+        public async Task<ActionResult> GenerateQRCode(string QRCodeText)
+        {
+            QRCodeGenerator _qrCode = new QRCodeGenerator();
+            QRCodeData qRCodeData = _qrCode.CreateQrCode(QRCodeText, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qRCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            //Set colors
+            //Bitmap qrCodeImage = qrCode.GetGraphic(20, "#000ff0", "#0ff000");
+
+            var bytes = ImageToByteArray(qrCodeImage);
+            return File(bytes, "image/bmp");
         }
     }
 }
