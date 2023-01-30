8c0cfbfb5115ad2a36f0a4883c0816f2411eebcb
jacqueline <D00233669@student.dkit.ie>
2023-01-30 21:52:20 +0000

    43193 updated cert read full dto

--- QardlessAPI/QardlessAPI/Data/Dtos/Certificate/CertificateReadFullDto.cs
+++ QardlessAPI/QardlessAPI/Data/Dtos/Certificate/CertificateReadFullDto.cs
@@ -3,12 +3,11 @@
     public class CertificateReadFullDto
     {
         public Guid Id { get; set; }
-        public string Title { get; set; }
-        public string QrCodeUri { get; set; }
-        public string PdfUri { get; set; }
-        public string SerialNumber { get; set; }
-        public bool Expires { get; set; }
+        public string CourseTitle { get; set; }
+        public string CertNumber { get; set; }
+        public DateTime CourseDate { get; set; }
         public DateTime ExpiryDate { get; set; }
+        public string PdfUrl { get; set; }
         public DateTime CreatedDate { get; set; }
         public Guid EndUserId { get; set; }
         public Guid BusinessId { get; set; }
