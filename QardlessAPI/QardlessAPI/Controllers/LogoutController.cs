a78a65560c50159b3c376f2205ee8377d625d192
jacqueline <D00233669@student.dkit.ie>
2023-02-09 18:20:00 +0000

    44284 added logout controller, tested on swagger

---
+++ QardlessAPI/QardlessAPI/Controllers/LogoutController.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QardlessAPI.Data;
+using QardlessAPI.Data.Dtos.EndUser;
+
+namespace QardlessAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogoutController : ControllerBase
+    {
+        private readonly IQardlessAPIRepo _repo;
+        private readonly IMapper _mapper;
+
+        public LogoutController(IQardlessAPIRepo repo, IMapper mapper)
+        {
+            _repo = repo ??
+                throw new ArgumentNullException(nameof(repo));
+            _mapper = mapper ??
+               throw new ArgumentNullException(nameof(mapper));
+        }
+
+        // PUT: api/Logout
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEndUser(Guid id, EndUserReadPartialDto endUserForLogout)
+        {
+            if (endUserForLogout == null)
+                return BadRequest();
+
+            var endUser = await _repo.GetEndUser(id);
+            if (endUser == null)
+                return NotFound();
+
+            endUserForLogout.isLoggedin = false;
+
+            _repo.SaveChanges();
+
+            return Ok(endUserForLogout);
+        }
+
+    }
+}
